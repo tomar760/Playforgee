e78f6bd6f7813c5371cc83e5b587cb7fe2155951
tomar760 <tomaraditya545@gmail.com>
2025-07-28 12:22:35 +0530

    Update firebase-config.js

--- dist/firebase-config.js
+++ dist/firebase-config.js
@@ -1,1 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnuhWybguIiv0IInLqL4fAz2lUR7KoBHA",
+  authDomain: "playforgeee.firebaseapp.com",
+  databaseURL: "https://playforgeee-default-rtdb.firebaseio.com",
+  projectId: "playforgeee",
+  storageBucket: "playforgeee.appspot.com",
+  messagingSenderId: "1018790311417",
+  appId: "1:1018790311417:web:00c3f038531a7e28c26a9f"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
