e3eaa8a668387a994bf8c6c57627c2dd97c82524
tomar760 <tomaraditya545@gmail.com>
2025-07-29 14:00:01 +0530

    Update firebase-config.js

--- dist/firebase-config.js
+++ dist/firebase-config.js
@@ -1,14 +1,9 @@
-// firebase-config.js
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDcJdokra81YJFijCzH3EvUpgjcbj7P9o0",
   authDomain: "playforgeee.firebaseapp.com",
+  databaseURL: "https://playforgeee-default-rtdb.firebaseio.com",
   projectId: "playforgeee",
-  DatabaseURL: "https://playforgeee-default-rtdb.firebaseio.com",
   storageBucket: "playforgeee.appspot.com",
   messagingSenderId: "440850239809",
-  appId: "1:440850239809:web:5795270644cdb1437ed1c0",
-  measurementId: "G-Y8S30GCX80"
+  appId: "1:440850239809:web:5795270644cdb1437ed1c0"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database();
