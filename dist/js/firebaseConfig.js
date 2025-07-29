e530ffbe660b54b9e18586fe58979afd13d24cd7
tomar760 <tomaraditya545@gmail.com>
2025-07-29 18:21:19 +0530

    Create firebaseConfig.js

---
+++ dist/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcJdokra81YJFijCzH3EvUpgjcbj7P9o0",
+  authDomain: "playforgeee.firebaseapp.com",
+  databaseURL: "https://playforgeee-default-rtdb.firebaseio.com",
+  projectId: "playforgeee",
+  storageBucket: "playforgeee.appspot.com",
+  messagingSenderId: "440850239809",
+  appId: "1:440850239809:web:5795270644cdb1437ed1c0"
+};
