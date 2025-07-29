961aff954d1df9ae3f4a2aab569c91ab3d3f7ab0
tomar760 <tomaraditya545@gmail.com>
2025-07-29 13:58:07 +0530

    Delete dist/admin.js

--- dist/admin.js
+++
@@ -1,9 +0,0 @@
-function updateCoins() {
-  const uid = document.getElementById("userId").value;
-  const coins = parseInt(document.getElementById("coins").value);
-  db.ref("users/" + uid).update({ coins }).then(() => {
-    document.getElementById("status").innerText = "Coins updated!";
-  }).catch(err => {
-    document.getElementById("status").innerText = err.message;
-  });
-}
